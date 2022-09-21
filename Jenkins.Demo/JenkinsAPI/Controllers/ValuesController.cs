105d637f4c54e67313a09ed5e1f9157ea38412cf
anxi.wan <287079054@qq.com>
2022-09-21 16:50:33 +0800

    新增项目

---
+++ Jenkins.Demo/JenkinsAPI/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("eleven")]
+        public string GetHtml()
+        {
+            return "abc";
+        }
+    }
+}
